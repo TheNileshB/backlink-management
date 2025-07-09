6418cdc13931b6255a30ce930a123abedb31933b
Nilesh Bambhaniya <nileshsci123@gmail.com>
2025-07-10 02:32:45 +0530

    Update admin-login.php

--- admin-login.php
+++ admin-login.php
@@ -61,7 +61,7 @@ if (isset($_POST['login']))
 <!DOCTYPE html>
 <html>
 <head>
-	<title>User Login</title>
+	<title>Admin Login</title>
 	<link rel="icon" href="fashionaminoo/images/favicon.png">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
