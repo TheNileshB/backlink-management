9cfab9f7f6982ee8d2f314c81d3f938764185667
Nilesh Bambhaniya <nileshsci123@gmail.com>
2025-07-09 07:06:07 +0530

    Update db-connect.php

--- fashionaminoo/db-connect.php
+++ fashionaminoo/db-connect.php
@@ -1,8 +1,6 @@
-<?php  
-
+<?php 
 $servername = "localhost";
-$username = "back_backlink_management_24578";
-$password = "EEAFeKkAvp!rcIPl";
-$dbname = "back_backlink_management"
-
+$username = "root";
+$password = "";
+$dbname = "backlink_management"
 ?>
